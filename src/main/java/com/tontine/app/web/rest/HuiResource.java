5b93cff1381341387c8e650aca2ed0c015f3a817
duoc.dam <tienduocict@gmail.com>
2023-04-08 11:51:00 +0700

    Delete hui message

--- src/main/java/com/tontine/app/web/rest/HuiResource.java
+++ src/main/java/com/tontine/app/web/rest/HuiResource.java
@@ -4,24 +4,24 @@ import com.tontine.app.domain.Hui;
 import com.tontine.app.repository.HuiRepository;
 import com.tontine.app.service.HuiService;
 import com.tontine.app.web.rest.errors.BadRequestAlertException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import tech.jhipster.web.util.HeaderUtil;
 import tech.jhipster.web.util.PaginationUtil;
 import tech.jhipster.web.util.ResponseUtil;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * REST controller for managing {@link com.tontine.app.domain.Hui}.
@@ -50,7 +50,8 @@ public class HuiResource {
      * {@code POST  /huis} : Create a new hui.
      *
      * @param hui the hui to create.
-     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new hui, or with status {@code 400 (Bad Request)} if the hui has already an ID.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new hui, or with status {@code 400 (Bad Request)} if
+     * the hui has already an ID.
      * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
     @PostMapping("/huis")
@@ -69,16 +70,14 @@ public class HuiResource {
     /**
      * {@code PUT  /huis/:id} : Updates an existing hui.
      *
-     * @param id the id of the hui to save.
+     * @param id  the id of the hui to save.
      * @param hui the hui to update.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated hui,
      * or with status {@code 400 (Bad Request)} if the hui is not valid,
      * or with status {@code 500 (Internal Server Error)} if the hui couldn't be updated.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
     @PutMapping("/huis/{id}")
-    public ResponseEntity<Hui> updateHui(@PathVariable(value = "id", required = false) final Long id, @RequestBody Hui hui)
-        throws URISyntaxException {
+    public ResponseEntity<Hui> updateHui(@PathVariable(value = "id", required = false) final Long id, @RequestBody Hui hui) {
         log.debug("REST request to update Hui : {}, {}", id, hui);
         if (hui.getId() == null) {
             throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
@@ -101,7 +100,7 @@ public class HuiResource {
     /**
      * {@code PATCH  /huis/:id} : Partial updates given fields of an existing hui, field will ignore if it is null
      *
-     * @param id the id of the hui to save.
+     * @param id  the id of the hui to save.
      * @param hui the hui to update.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated hui,
      * or with status {@code 400 (Bad Request)} if the hui is not valid,
@@ -168,10 +167,12 @@ public class HuiResource {
     @DeleteMapping("/huis/{id}")
     public ResponseEntity<Void> deleteHui(@PathVariable Long id) {
         log.debug("REST request to delete Hui : {}", id);
+        Optional<Hui> hui = huiService.findOne(id);
         huiService.delete(id);
-        return ResponseEntity
-            .noContent()
-            .headers(HeaderUtil.createEntityDeletionAlert(applicationName, false, ENTITY_NAME, id.toString()))
-            .build();
+        return hui
+            .<ResponseEntity<Void>>map(value ->
+                ResponseEntity.noContent().headers(HeaderUtil.createAlert(applicationName, "Đã xóa hụi: ", value.getTenHui())).build()
+            )
+            .orElse(null);
     }
 }
