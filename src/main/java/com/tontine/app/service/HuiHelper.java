de2291566691d0e44af2abceb981dd0ab4e2a3c2
duoc.dam <tienduocict@gmail.com>
2023-06-30 10:27:20 +0700

    Fix tien hot hui 14.500.000

--- src/main/java/com/tontine/app/service/HuiHelper.java
+++ src/main/java/com/tontine/app/service/HuiHelper.java
@@ -2,21 +2,24 @@ package com.tontine.app.service;
 
 import com.tontine.app.domain.ChiTietHui;
 import com.tontine.app.domain.Hui;
+import java.util.Objects;
 
 public class HuiHelper {
 
     public static Long calculateTienHotHui(ChiTietHui chiTietHui) {
         Long thamKeu = chiTietHui.getThamKeu();
-        if (thamKeu == null) {
-            return null;
-        }
         Hui hui = chiTietHui.getHui();
         Integer soPhan = hui.getSoPhan();
         Long dayHui = hui.getDayHui();
         Integer ky = chiTietHui.getKy();
+
+        if (thamKeu == null || ky == null) {
+            return null;
+        }
+
         if (ky == 1) { // Hot hui dau
             return (soPhan - 1) * (dayHui - thamKeu) - (dayHui / 2);
-        } else if (ky == soPhan - 1) { // Hot hui chot
+        } else if (Objects.equals(ky, soPhan)) { // Hot hui chot
             return dayHui * ky - (dayHui / 2);
         } else {
             return (ky - 1) * dayHui + (soPhan - ky) * (dayHui - thamKeu) - (dayHui / 2);
