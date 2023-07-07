6e705c42a5a41e2ee45bfa31a42686964fe42f59
duoc.dam <tienduocict@gmail.com>
2023-07-08 02:05:20 +0700

    Revert "Auxiliary commit to revert individual files from a1ac725299123f68d71abfe3a47dccb18534d0b8"
    This reverts commit d94e3432d4169e465af74e77131afe56cdc932ec.

--- src/main/java/com/tontine/app/service/HuiHelper.java
+++ src/main/java/com/tontine/app/service/HuiHelper.java
@@ -1,28 +1,28 @@
 package com.tontine.app.service;
 
 import com.tontine.app.domain.ChiTietHui;
+import com.tontine.app.domain.Hui;
+import java.util.Objects;
 
 public class HuiHelper {
 
     public static Long calculateTienHotHui(ChiTietHui chiTietHui) {
         Long thamKeu = chiTietHui.getThamKeu();
+        Hui hui = chiTietHui.getHui();
+        Integer soPhan = hui.getSoPhan();
+        Long dayHui = hui.getDayHui();
         Integer ky = chiTietHui.getKy();
+
         if (thamKeu == null || ky == null) {
             return null;
         }
 
-        //        Hui hui = chiTietHui.getHui();
-        //        Integer soPhan = hui.getSoPhan();
-        //        Long dayHui = hui.getDayHui();
-        //
-        //        if (ky == 1) { // Hot hui dau
-        //            return (soPhan - 1) * (dayHui - thamKeu) - (dayHui / 2);
-        //        } else if (Objects.equals(ky, soPhan)) { // Hot hui chot
-        //            return dayHui * ky - (dayHui / 2);
-        //        } else {
-        //            return (ky - 1) * dayHui + (soPhan - ky) * (dayHui - thamKeu) - (dayHui / 2);
-        //        }
-
-        return chiTietHui.getTienHot();
+        if (ky == 1) { // Hot hui dau
+            return (soPhan - 1) * (dayHui - thamKeu) - (dayHui / 2);
+        } else if (Objects.equals(ky, soPhan)) { // Hot hui chot
+            return dayHui * ky - (dayHui / 2);
+        } else {
+            return (ky - 1) * dayHui + (soPhan - ky) * (dayHui - thamKeu) - (dayHui / 2);
+        }
     }
 }
