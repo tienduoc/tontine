920b5946ebf2d0a0c37a2e4a016675a9154931bc
khoi.hlt <huynhlutrongkhoi@gmail.com>
2024-08-04 11:39:29 +0700

    update show thong tin hụi tham gia

--- src/main/webapp/app/modules/hui-vien/detail/hui-vien-detail.component.ts
+++ src/main/webapp/app/modules/hui-vien/detail/hui-vien-detail.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { IHuiVien } from '../hui-vien.model';
 import { HuiService, ResThongKe } from 'app/entities/hui/service/hui.service';
 import { filter, map } from 'rxjs/operators';
+import { sumBy } from 'lodash';
 
 @Component({
   selector: 'jhi-hui-vien-detail',
@@ -14,22 +15,39 @@ export class HuiVienDetailComponent implements OnInit {
   huiVien: IHuiVien | null = null;
   thongKe!: ResThongKe;
 
-  constructor(protected activatedRoute: ActivatedRoute, private huiService: HuiService) {}
+  constructor(protected activatedRoute: ActivatedRoute, private huiService: HuiService) {
+    this.activatedRoute.data.subscribe(({ huiVien }) => (this.huiVien = huiVien));
+  }
 
   ngOnInit(): void {
-    this.activatedRoute.data.subscribe(({ huiVien }) => (this.huiVien = huiVien));
+    console.log(this.huiVien);
 
     this.huiVien?.chiTietHuis.map((ctHuis: any) => {
       const idHui = ctHuis.hui.id;
+      ctHuis.isHuiSong = ctHuis.tienHot !== null;
+
       this.huiService.find(idHui).subscribe(data => {
         const maxKy = this.timKilonnhat((data?.body as any).chiTietHuis);
         ctHuis.maxKy = maxKy;
+        if (ctHuis.tienHot !== null) {
+          ctHuis.tienHuiSong = ctHuis.hui.dayHui * ctHuis.maxKy;
+        } else {
+          ctHuis.tienHuiChet = ctHuis.hui.dayHui * (ctHuis.hui.soPhan - (ctHuis.ky - 1));
+        }
       });
 
       return ctHuis;
     });
 
     this.getThongKe();
+  }
+
+  calculateTotalHuiSong(chiTietHuis: any) {
+    return sumBy(chiTietHuis, 'tienHuiSong');
+  }
+
+  calculateTotalHuiChet(chiTietHuis: any) {
+    return sumBy(chiTietHuis, 'tienHuiChet');
   }
 
   timKilonnhat(chiTietHuis: any): number {
