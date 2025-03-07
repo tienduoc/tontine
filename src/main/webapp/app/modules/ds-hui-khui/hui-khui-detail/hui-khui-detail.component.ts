936e9f0509504e23506abf55fa16a6377d5ad9bc
khoi.hlt <huynhlutrongkhoi@gmail.com>
2025-03-07 11:53:49 +0700

    mock ui

--- src/main/webapp/app/modules/ds-hui-khui/hui-khui-detail/hui-khui-detail.component.ts
+++ src/main/webapp/app/modules/ds-hui-khui/hui-khui-detail/hui-khui-detail.component.ts
@@ -5,79 +5,31 @@ import { MatDialog } from '@angular/material/dialog';
 
 import { TinhTienPopupComponent } from 'app/components/tinh-tien-popup/tinh-tien-popup.component';
 import { IHui } from '../../hui/hui.model';
+import { HttpClient } from '@angular/common/http';
+import { ApplicationConfigService } from 'app/core/config/application-config.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'jhi-hui-khui-detail',
   templateUrl: './hui-khui-detail.component.html',
   styleUrls: ['./hui-khui-detail.component.scss'],
 })
-export class HuiKhuiDetailComponent implements OnInit, OnDestroy {
-  @ViewChild('screen', { static: true }) screen: any;
+export class HuiKhuiDetailComponent {
+  protected resourceUrl = this.applicationConfigService.getEndpointFor('api/ds-hui-khui');
 
-  hui: IHui | null = null;
-
-  predicate = 'id';
-
-  ascending = true;
-
-  chiTietHuis: any;
-
-  ki = 0;
-
-  constructor(private dialog: MatDialog, protected activatedRoute: ActivatedRoute) {}
-
-  ngOnDestroy(): void {
-    (document.querySelector('.card-global') as any).style.display = 'block';
+  constructor(
+    protected http: HttpClient,
+    protected applicationConfigService: ApplicationConfigService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.getDsHuiKhui();
   }
 
-  ngOnInit(): void {
-    this.activatedRoute.data.subscribe(({ hui }) => {
-      this.hui = hui;
-      this.chiTietHuis = sortBy([...hui.chiTietHuis], ['huiVien.hoTen', 'ky']);
-    });
+  getDsHuiKhui(): void {
+    const id = this.activatedRoute.snapshot.params.id;
 
-    this.getKiLonNhat();
-
-    setTimeout(() => {
-      (document.querySelector('.card-global') as any).style.display = 'none';
-    }, 0);
-  }
-
-  getKiLonNhat(): void {
-    (this.hui?.chiTietHuis || []).forEach((item: any) => {
-      if (item?.ky > this.ki) {
-        this.ki = item?.ky;
-      }
-    });
-  }
-
-  navigateToWithComponentValues(): void {}
-
-  public getColorByKi(itemKi: number): string {
-    if (itemKi === this.ki) {
-      return '#FE0002';
-    } else if (itemKi) {
-      return '#008000';
-    } else {
-      return '#FFFFFF';
-    }
-  }
-
-  xemTinhTien(chiTietHui: any): void {
-    const idChiTietHui = chiTietHui?.id;
-
-    if (!idChiTietHui) {
-      return;
-    }
-
-    this.dialog.open(TinhTienPopupComponent, {
-      height: '100%',
-      width: '100%',
-      maxHeight: '100%',
-      maxWidth: '100%',
-      data: {
-        idChiTietHui,
-      },
+    this.http.get<any>(`${this.resourceUrl}/user/${id}`, { observe: 'response' }).subscribe(data => {
+      console.log(data);
     });
   }
 }
